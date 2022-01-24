435fd2f3d280b6403bc4d9adbd7782b15961af37
ScibikSzymon <scibik.szym@gmail.com>
2022-01-24 15:55:18 +0100

    add prism modules

---
+++ Patterns/MailContextModule/MailContextModule.cs
@@ -0,0 +1,19 @@
+﻿using Prism.Ioc;
+using Prism.Modularity;
+using System;
+
+namespace MailContextModule
+{
+    public class MailContextModule : IModule
+    {
+        public void OnInitialized(IContainerProvider containerProvider)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Patterns/MailListModule/MailListModule.cs
@@ -0,0 +1,19 @@
+﻿using Prism.Ioc;
+using Prism.Modularity;
+using System;
+
+namespace MailListModule
+{
+    public class MailListModule : IModule
+    {
+        public void OnInitialized(IContainerProvider containerProvider)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
